ce3b1c7e5567ec5291de90e0b2a8db5f652ccbff
RazelRaz <razel16dcm@gmail.com>
2023-07-13 20:46:11 +0600

    useState Understading Inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import CachingExpensiveComputation from "./components/CachingExpensiveComputation"
-import PersistedMutableProperty from "./components/PersistedMutableProperty"
-import UseRefWorkingWithCSS from "./components/UseRefWorkingWithCSS"
-import UseRefWorkingWithInput from "./components/UseRefWorkingWithInput"
-import UserefAttributeValue from "./components/UserefAttributeValue"
-import UserefInnerText from "./components/UserefInnerText"
+// import CachingExpensiveComputation from "./components/CachingExpensiveComputation"
+// import PersistedMutableProperty from "./components/PersistedMutableProperty"
+// import UseRefWorkingWithCSS from "./components/UseRefWorkingWithCSS"
+// import UseRefWorkingWithInput from "./components/UseRefWorkingWithInput"
+// import UserefAttributeValue from "./components/UserefAttributeValue"
+// import UserefInnerText from "./components/UserefInnerText"
+
+import UseStateUnderstadingInside from "./components/UseStateUnderstadingInside"
 
 function App() {
 
   return (
     <>
       <h1>Meet With hook</h1>
-      <UserefInnerText></UserefInnerText>
+      {/* <UserefInnerText></UserefInnerText>
       <UserefAttributeValue></UserefAttributeValue>
       <UseRefWorkingWithInput></UseRefWorkingWithInput>
       <UseRefWorkingWithCSS></UseRefWorkingWithCSS>
       <PersistedMutableProperty></PersistedMutableProperty>
-      <CachingExpensiveComputation></CachingExpensiveComputation>
+      <CachingExpensiveComputation></CachingExpensiveComputation> */}
+      <UseStateUnderstadingInside></UseStateUnderstadingInside>
     </>
   )
 }
 
 export default App
+
---
+++ src/components/UseStateUnderstadingInside.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const UseStateUnderstadingInside = () => {
+
+    const [number, setNumber] = useState(0)
+
+    return (
+        <div>
+            <h1>useState Understading Inside</h1>
+            <br /><br />
+            <h6>Number:{number}</h6>
+            <button onClick={()=>setNumber(number+1)}>Click To Increase Number</button>
+            <button onClick={()=>setNumber(0)}>Reset Number</button>
+        </div>
+    );
+};
+
+export default UseStateUnderstadingInside;
